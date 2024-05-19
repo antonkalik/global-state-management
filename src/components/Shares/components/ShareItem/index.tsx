9f4d1ccb2d1c31e723be782addf3d539b04c2809
Anton Kalik <antonkalik@gmail.com>
2024-05-19 21:33:50 +0200

    share item

--- src/components/Shares/components/ShareItem/index.tsx
+++ src/components/Shares/components/ShareItem/index.tsx
@@ -1,31 +1,27 @@
 import React from "react";
 
-export const ShareItem = React.memo(
-  ({
-    symbol,
-    price,
-    onUpdate,
-  }: {
+type ShareItemProps = {
     symbol: string;
     price: number;
     onUpdate: (symbol: string) => void;
-  }) => {
-    console.log(`ShareItem ${symbol} updated`);
+}
 
-    return (
-      <li key={price + symbol}>
-        <p>
-          {symbol} : {price}
-        </p>
-        <button
-          type="button"
-          onClick={() => {
-            onUpdate(symbol);
-          }}
-        >
-          Increase
-        </button>
-      </li>
-    );
-  },
-);
+export const ShareItem = React.memo(({ symbol, price, onUpdate }: ShareItemProps) => {
+  console.log(`ShareItem ${symbol} updated`);
+
+  return (
+    <li key={price + symbol}>
+      <p>
+        {symbol} : {price}
+      </p>
+      <button
+        type="button"
+        onClick={() => {
+          onUpdate(symbol);
+        }}
+      >
+        Increase
+      </button>
+    </li>
+  );
+});
