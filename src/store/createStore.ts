b3d6f6ff3c33a1a5aa5f14ee205da0e8ba8f9129
Anton Kalik <antonkalik@gmail.com>
2024-05-19 21:49:20 +0200

    simple store

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -3,20 +3,15 @@ import { useSyncExternalStore } from "react";
 export function createStore<S>(initialStore: S) {
   const listeners = new Set<(store: S) => void>();
   let state = initialStore;
-  let isInitialized = false;
 
   return {
-    init: (initState: S) => {
-      if (!isInitialized) {
-        state = initState;
-        isInitialized = true;
-      }
-    },
     setState(callback: (store: S) => S) {
       state = callback(state);
       listeners.forEach((listener) => listener(state));
     },
-    useSelector<SelectorData>(selector: (store: S) => SelectorData): SelectorData {
+    useSelector<SelectorData>(
+      selector: (store: S) => SelectorData,
+    ): SelectorData {
       return useSyncExternalStore(
         (listener) => {
           listeners.add(listener);
